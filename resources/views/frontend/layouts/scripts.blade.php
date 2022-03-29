96858480b42e31c028ee9da23b3c02dd20eadb45
Baba NGOM <48138670+alameenjr7@users.noreply.github.com>
2022-03-29 12:56:52 +0000

    1_test deployed

--- resources/views/frontend/layouts/scripts.blade.php
+++ resources/views/frontend/layouts/scripts.blade.php
@@ -62,5 +62,14 @@
         @endif
 
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3183523459637784" crossorigin="anonymous"></script>
-
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3183523459637784" crossorigin="anonymous"></script>
+        <ins class="adsbygoogle"
+                style="display:block; text-align:center;"
+                data-ad-layout="in-article"
+                data-ad-format="fluid"
+                data-ad-client="ca-pub-3183523459637784"
+                data-ad-slot="7549265885"></ins>
+        <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         @yield('scripts')
