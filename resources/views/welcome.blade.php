444a781cc77a43176a575e0864c0a5c445e5837e
Baba NGOM <48138670+alameenjr7@users.noreply.github.com>
2022-03-30 19:46:03 +0000

    1_test deployed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.master')
 
 @section('title')
-    <title>{{get_setting('title')}}: Création site Web, Applications, Logiciels</title>
+    <title>{{get_setting('title')}} - Création site Web, Applications, Logiciels</title>
 @endsection
 
 @section('content')
